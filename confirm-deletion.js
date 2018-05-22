bd41a79c593a4044df803916a359957a91aecffb
Mike Nicholaides <mike.nicholaides@gmail.com>
2018-05-22 13:24:45 -0400

    Use confirmation dialog before deleting the users key

---
+++ confirm-deletion.js
@@ -0,0 +1,18 @@
+function run(argv) {
+  const app = Application.currentApplication()
+  app.includeStandardAdditions = true
+
+  yesButton = 'Delete'
+  noButton = 'Cancel'
+
+  const answer = app.displayDialog('Are you sure you want to delete this file?', {
+    buttons: [noButton, yesButton],
+    defaultButton: noButton,
+    cancelButton: noButton
+  })
+
+  if (answer.buttonReturned == yesButton) {
+    console.log('command:delete')
+    return 'command:delete'
+  }
+}
